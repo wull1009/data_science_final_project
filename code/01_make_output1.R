5b1d7365856e66f6c263f9f0d8fcba5c33fce360
Linlin Wu <142555452+wull1009@users.noreply.github.com>
2024-11-14 19:43:43 -0500

    Update 01_make_output1.R

--- code/01_make_output1.R
+++ code/01_make_output1.R
@@ -1,5 +1,3 @@
-#! TO DO:
-#!   add call to here::i_am
 here::i_am("code/01_make_output1.R")
 
 library(gtsummary)
@@ -7,8 +5,6 @@ lm1 <- lm(charges ~ as.factor(smoker) + age + sex + bmi + children + v1 + v2 + v
 tbl_regression(lm1)
 table1=tbl_regression(lm1)
 
-#! TO DO: 
-#!   save random_numbers1 in output1 directory
 saveRDS(
   table1, 
   file = here::here("output1/output1.rds")
