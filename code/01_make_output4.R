d9acf8322efda036dba63c9de92612ee2638c62c
Linlin Wu <142555452+wull1009@users.noreply.github.com>
2024-11-14 19:45:27 -0500

    Update 01_make_output4.R

--- code/01_make_output4.R
+++ code/01_make_output4.R
@@ -1,5 +1,3 @@
-#! TO DO:
-#!   add call to here::i_am
 here::i_am("code/01_make_output4.R")
 
 library(ggplot2)
@@ -10,8 +8,6 @@ p1<-ggplot(xy, aes(age, fill = sex)) + geom_density(alpha = 0.2)+ggtitle("Fig.2 
 p2<-ggplot(xy, aes(bmi, fill = sex)) + geom_density(alpha = 0.2)+ggtitle("BMI frequency distribution plot by gender.")+ theme(plot.title = element_text(hjust = 0.5))
 fig2=grid.arrange(p1,p2)
 
-#! TO DO: 
-#!   save random_numbers1 in output1 directory
 ggplot2::ggsave(
   fig2, 
   file = here::here("output4/output4.png")
