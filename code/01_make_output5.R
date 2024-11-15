9c43672ef25086a8705cd4933ff9b33eb4e889e2
Linlin Wu <142555452+wull1009@users.noreply.github.com>
2024-11-14 19:45:39 -0500

    Update 01_make_output5.R

--- code/01_make_output5.R
+++ code/01_make_output5.R
@@ -1,13 +1,9 @@
-#! TO DO:
-#!   add call to here::i_am
 here::i_am("code/01_make_output5.R")
 
 library(ggplot2)
 fig3=ggplot(data=xy,aes(x=children,fill=sex))+geom_histogram(alpha=0.8,position = "dodge",bins = 10)+ggtitle("Fig.3 Distribution plot of the number of children by gender.")+theme(plot.title = element_text(hjust = 0.5))+ 
   labs(x = "Number of children/dependents", y = "Count")
 
-#! TO DO: 
-#!   save random_numbers1 in output1 directory
 ggplot2::ggsave(
   fig3, 
   file = here::here("output5/output5.png")
