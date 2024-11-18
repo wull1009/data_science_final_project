469ca5741018521d9df7176d096a1689f57b65bc
Linlin Wu <142555452+wull1009@users.noreply.github.com>
2024-11-17 23:49:30 -0500

    Update 01_make_output2.R

--- code/01_make_output2.R
+++ code/01_make_output2.R
@@ -1,5 +1,7 @@
 here::i_am("code/01_make_output2.R")
 
+a1=read.csv(file=here::here("data_smoke.csv"))
+
 lm2=lm(charges~smoker+age+sex+bmi+children+v1+v2+v3,data=a1)
 lm.aic=step(lm2,trace=F)
 library(gtsummary)
