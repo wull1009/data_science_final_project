1d92444b7e0b5fd21cabb067457e2bd4d87b1f36
Linlin Wu <142555452+wull1009@users.noreply.github.com>
2024-11-17 23:53:36 -0500

    Update 01_make_output5.R

--- code/01_make_output5.R
+++ code/01_make_output5.R
@@ -1,5 +1,6 @@
 here::i_am("code/01_make_output5.R")
-
+a=read.csv(file=here::here("data_smoke.csv"))
+xy<-data.frame(a)
 library(ggplot2)
 fig3=ggplot(data=xy,aes(x=children,fill=sex))+geom_histogram(alpha=0.8,position = "dodge",bins = 10)+ggtitle("Fig.3 Distribution plot of the number of children by gender.")+theme(plot.title = element_text(hjust = 0.5))+ 
   labs(x = "Number of children/dependents", y = "Count")
