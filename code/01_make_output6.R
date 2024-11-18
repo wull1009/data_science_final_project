cf878aa4f58bcb403e4c11d0f0847c615c510b3d
Linlin Wu <142555452+wull1009@users.noreply.github.com>
2024-11-17 23:54:12 -0500

    Update 01_make_output6.R

--- code/01_make_output6.R
+++ code/01_make_output6.R
@@ -1,5 +1,5 @@
 here::i_am("code/01_make_output6.R")
-
+a=read.csv(file=here::here("data_smoke.csv"))
 library(ggplot2)
 fig4=ggplot(a, aes(x=smoker, y=charges,col=sex)) + geom_boxplot()+
   ggtitle("Fig.4 Plot of the relationship between smoking and insurance charges")+theme(plot.title = element_text(hjust = 0.5))
