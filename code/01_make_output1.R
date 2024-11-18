eb2705fbef5588fd5061aa1f563834084b9a3e5f
Linlin Wu <142555452+wull1009@users.noreply.github.com>
2024-11-17 23:48:37 -0500

    Update 01_make_output1.R

--- code/01_make_output1.R
+++ code/01_make_output1.R
@@ -1,5 +1,7 @@
 here::i_am("code/01_make_output1.R")
 
+data=read.csv(file=here::here("data_smoke.csv"))
+
 library(gtsummary)
 lm1 <- lm(charges ~ as.factor(smoker) + age + sex + bmi + children + v1 + v2 + v3, data = a1)
 tbl_regression(lm1)
